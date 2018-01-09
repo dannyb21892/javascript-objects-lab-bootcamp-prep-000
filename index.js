a45970caa5b2a6ce0001f1e7d597f33d888bcaf8
Daniel Baamonde <dannyb21892@gmail.com>
2018-01-09 05:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newobject = Object.assign({}, object, tempobj)
   return newobject
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
